9b5ab3d16c498b37b52d346af44536c59ba993ec
HectorBonilhaa <hectoroliveira1@gmail.com>
2022-11-01 18:25:30 -0300

    exercicio Todo List

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Task } from "./components/Tasks";
+import { Home } from "./pages/Home";
+import { GlobalStyle } from "./styles/global";
+
+ export function App() {
+
+
+  return (
+    <>
+    <GlobalStyle />
+    <Home/>
+    <Task/>
+    
+    </>
+  );
+}
+
+
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+
+import {Container} from "./styled"
+
+
+export function Home(){
+  
+return (
+    <Container>
+      <h1 className="title">TODO LIST</h1>
+     
+    </Container>
+  );
+}
